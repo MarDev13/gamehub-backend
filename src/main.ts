f8358c94d54d6f885dc757b763dd1a11c1887fe8
MarDev13 <mardevvdesign@gmail.com>
2025-10-11 20:29:52 +0200

    chore(core): setup inicial del backend NestJS (configuración base, CORS, validación, logger y estructura modular)

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,36 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe, Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  const env = process.env.NODE_ENV || 'development';
+  const port = Number(process.env.PORT) || 3000;
+
+  if (env === 'development') {
+    app.enableCors();
+    console.log('CORS enabled for all origins in development mode');
+  } else {
+    app.enableCors({
+      origin: '',
+      credentials: true,
+    });
+    console.log('CORS enabled for specific orgin in production mode');
+  }
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: false,
+      transform: true,
+    }),
+  );
+
+  await app.listen(port);
+  const logger = new Logger('Bootstrap');
+  logger.log(`Application running on port ${port} in ${env} mode`);
 }
-bootstrap();
+bootstrap().catch((error) => {
+  console.error('Error during application bootstrap:', error);
+});
