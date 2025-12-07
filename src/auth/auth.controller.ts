b4b53114a3d99cff1ca0498356a15169da59421b
MarDev13 <mardevvdesign@gmail.com>
2025-12-08 00:28:25 +0100

    feat(auth): implement JWT authentication, login/register endpoints, JWT strategy, JwtAuthGuard and /auth/me protected route

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,8 @@
-import { Body, Controller, Post } from "@nestjs/common";
+import { Body, Controller, Post, UseGuards, Get, Req } from "@nestjs/common";
 import { AuthService } from "./auth.service";
 import { LoginDto } from "./dto/login.dto";
 import { RegisterDto } from "./dto/register.dto";
+import { JwtAuthGuard } from "./guards/jwt-auth.guard";
 
 @Controller('auth')
 export class AuthController {
@@ -14,4 +15,9 @@ export class AuthController {
   login(@Body() dto: LoginDto){
     return this.authService.login(dto);
   }
+  @UseGuards(JwtAuthGuard)
+  @Get('me')
+  getMe(@Req() req){
+    return req.user;
+}
 }
